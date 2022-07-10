4de543fbcbe8d1bed14586b59cdbeed67a697f54
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-07-10 23:06:31 +0200

    primo component

---
+++ exercises/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Helloworld } from "./HelloWorld";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <Helloworld />
+        )
+    }
+}
