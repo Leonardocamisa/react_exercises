a3d50b3ed6eb7b494d8a393620bf0b070948f92b
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-07-13 18:39:01 +0200

    esercizio da rivedere ma funzionante

--- exercises/src/Age.js
+++ exercises/src/Age.js
@@ -2,10 +2,10 @@ import React from "react"
 
 export class Age extends React.Component {
     render (){
-     return  (this.props.age ) ? <p>Your age is, {this.props.age}</p>  : <p></p>   
+     return  this.props.age < 65 && this.props.age > 18? <p>Your age is, {this.props.age}</p>  : <p></p>   
     }
 
-    // static defaultProps = {
-    //     age: 24,
-    // }
+     static defaultProps = {
+         age: 12,
+     }
 }
