c2239f559b42efd282bd59f3f79781f9921a644c
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-07-19 20:10:13 +0200

    esercizio completato

--- exercises/src/Login.js
+++ exercises/src/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-
 export default class Login extends React.Component {
 
         eventHandler = (event) => {
             this.setState(
                 {
-                    [event.target.name] : event.target.type === 'checkbox' ? event.target.checked : event.target.value
+                    [event.target.name] : event.target.type === "checkbox" ? event.target.checked : event.target.value,
+                    compiled: this.state.username !== "" && this.state.password !== "" ? true : false
                 }
             )
         }
@@ -14,22 +14,23 @@ export default class Login extends React.Component {
         state = {
             username : "",
             password : "",
-            remember : true
+            remember : true,
+            compiled : false
         }
 
         componentDidUpdate(){
             console.log (this.state)
         }
 
-    render (){
-        return(
-        <>
-        <div>
-            <h3>LOGIN</h3>
-            <input type="text" name="username" value = {this.state.username} onChange={this.eventHandler}/>
-            <input type="password" name="password" value = {this.state.password} onChange={this.eventHandler}/>
-            <input type="checkbox" name="remember" checked = {this.state.remember} onChange={this.eventHandler}/>
-        </div>
-        </>)
-    }
+        render (){
+            return(
+                <div>
+                    <h1>LOGIN NOW</h1>
+                    <input type="text" name="username" value={this.state.username} onChange={this.eventHandler} />
+                    <input type="password" name="password" value={this.state.password} onChange={this.eventHandler} />
+                    <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.eventHandler} />
+					<button onClick={() => this.props.onLogin(this.state)} disabled = {!this.state.compiled}>LOGIN</button>
+                </div>
+            )
+        }
 }
