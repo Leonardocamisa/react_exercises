d06177998f3bafa57a93ccf60e62e6aab3963ac2
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-07-27 20:32:30 +0200

    exercise fixed

--- exercises/src/TodoList.js
+++ exercises/src/TodoList.js
@@ -2,37 +2,38 @@ import React from "react";
 
 class TodoList extends React.Component {
 
-        state = {
-            items : ['Pino', 'Andrea', 'Willy', "Paola"],
-            newItem : ""
-        }
+    state = {
+        items : ['bere', 'mangiare', 'comprare vino', 'comprare sigarette', 'coding'],
+        newItems : ''
+    }
 
-        addEvent = () => {
+    addEvent = () => {
+        this.setState({
+            items: [...this.state.items, this.state.newItems],
+            newItems  : ''
+        })
+    }
 
-            this.setState({
-                items : [...this.state.items,this.state.newItem],
-                newItem : ""
-            })
-        }
-
-        addItem = (event) => {
-            this.setState({
-                newItem : event.target.value
-            })
-        }
+    addItem = (event) => {
+        this.setState({
+            newItem : event.target.value
+        })
+    }
 
     render(){
         return(
-        <>
-        <div>
-            <h1>TODO LIST</h1>
-            <input type="text" onChange={this.addItem} value = {this.state.newItem}/>
-            <button onClick={this.addEvent} disabled={!this.state.newItem}>ADD</button>
-            <ul>
-                {this.state.items.map((el,i) => <li key={el+i}>{el}</li>)}
-            </ul>
-        </div>
-        </>)
+            <>
+            <div>
+                <h1>Lista</h1>
+                <input type='text' onChange={this.addItem} value={this.state.newItem} />
+                <button onClick={this.addEvent} disabled={!this.state.newItem}>aggiungi</button>
+                <ul>
+                    {this.state.items.map((el,i) => <li key={el+i}>{el}</li>)}
+                </ul>
+            </div>
+
+            </>
+        )
     }
 }
 
