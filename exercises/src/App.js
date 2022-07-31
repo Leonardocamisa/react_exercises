c57ea58bf7d6426d5541e0c448db8d4f34e310ed
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-01 00:04:29 +0200

    exercises completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,18 +1,39 @@
 import React from "react";
-import Container from "./Container";
-import Counter from "./Counter";
+import TodoList from "./TodoList";
 
 class App extends React.Component {
-  render() {
-    return (
-        <>
-          <Container title='bellissimo titolo'>
-            <Counter />
-          </Container>
-        </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<div>
+					<TodoList
+						render={(items,newState,deleteState) => {
+							const itemsCopy = [...items.items];
+							return (
+								<ul className="list">
+									{itemsCopy.map((el, i) => (
+										<div key={i}>
+											<li>
+												{el}
+												<button
+													onClick={() => {deleteState(itemsCopy,i);newState(itemsCopy)}
+													}
+												>
+													CLEAR
+												</button>
+											</li>
+										</div>
+									))
+                }
+								</ul>
+							);
+						}}
+					
+          setState = ""/>
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
-
