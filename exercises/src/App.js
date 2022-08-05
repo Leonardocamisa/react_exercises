621d795b698eda56cde5616a11967934700e9e58
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-05 23:19:41 +0200

    exercises completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
+
 import { DisplayLanguage } from "./DisplayLanguage";
+
 import { languageContext } from "./LanguageContext";
 
 export class App extends React.Component {
-	state = {
-		language: "en",
-	};
+  state = {
+    language: "en",
+  };
 
-	handleLenguageChange = (event) => {
-		this.setState({
-			language: event.target.value,
-		});
-		console.log(this.state);
-	};
+  handleLenguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+    console.log(this.state);
+  };
 
-	onLogin = (state) => console.log(state);
+  onLogin = (state) => console.log(state);
 
-	render() {
-		return (
-			<>
-				<languageContext.Provider value={this.state.language}>
-					<DisplayLanguage onChange={this.handleLenguageChange}/>
-				</languageContext.Provider>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <languageContext.Provider value={this.state.language}>
+          <DisplayLanguage onChange={this.handleLenguageChange} />
+        </languageContext.Provider>
+      </>
+    );
+  }
 }
