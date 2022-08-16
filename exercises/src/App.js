4807edfeb9dc55b7c2ae7882cddd282963c18f9a
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-16 20:21:13 +0200

    exercises fixed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helloworld from './HelloWorld'
 
-export class App extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <div>
