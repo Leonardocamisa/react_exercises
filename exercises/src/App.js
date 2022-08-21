b581fb8f490cd988deb1d377131af606b9859e3d
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-21 23:23:04 +0200

    exercises completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Welcome } from "./Welcome";
 import { Routes, Route } from "react-router-dom";
 import { ClickCounter } from "./Counter";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path="/" element={<Welcome name="ALE" />}></Route>
+          <Route path="/" element={<Welcome name="ALEEEE" />}></Route>
           <Route path="/counter" element={<ClickCounter />}></Route>
+          <Route path="users/:username" element={<ShowGithubUser />}/>
         </Routes>
       </div>
     </>
---
+++ exercises/src/ShowGithubUser.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { GithubUser } from "./GithubUser";
+
+export function ShowGithubUser(){
+
+ const {username} = useParams()
+
+ return(
+  <>
+  <div>
+  <GithubUser username = {username}/>
+  </div>
+  </>
+ )
+}
