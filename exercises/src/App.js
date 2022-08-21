7b1c83ce86fc7cbddd8452cb949baf7c21659376
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-21 20:55:11 +0200

    exercise fixed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import Counter from "./Counter";
+import React from 'react';
+import FilteredList from './FilteredList';
 
-function App() {
-  return (
-    <>
-      <div>
-        <Counter/>
-      </div>
-    </>
-  );}
+const App = () => {
+    const list = [
+        {id:45, name:"Leonardo", age: 25},
+        {id:87, name:"Pino", age: 120},
+        {id:12, name:"Antonio", age: 2},
+        {id:1, name:"Peppe", age: 10},
+    ]
+        return(
+            <div>
+                <FilteredList list={list} />
+            </div>
+        );
+};
 export default App;
