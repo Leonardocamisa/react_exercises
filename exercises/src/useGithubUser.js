794d3e8d6c4bacf3d8c221b00c63f11e9c71e3cb
Leonardocamisa <Leonardocamisa697@gmail.com>
2022-08-22 15:02:45 +0200

    exercise completed

---
+++ exercises/src/useGithubUser.js
@@ -0,0 +1,19 @@
+import useSWR from 'swr'
+
+const fetcher = url => fetch(url).then(response => response.json())
+
+export function useGitHubUser (username) {
+
+    const {data, error, mutate} = useSWR('https://api.github.com/user', fetcher)
+
+    function fetchGitHubUser() {
+        mutate()
+    }
+
+    return {
+        data,
+        error,
+        loading: !data && !error,
+        onFetch: fetchGitHubUser
+    }
+}
